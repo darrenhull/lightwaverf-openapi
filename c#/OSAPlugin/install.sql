b0b7192189e739676949b6a27fc22f2fff663e81
steve.aitken@gmail.com <steve.aitken@gmail.com@68067983-10d6-bc12-f836-432b7b55e70a>
2012-09-19 21:53:57 +0000

    Added OpenSourceAutomation plugin

---
+++ c#/OSAPlugin/install.sql
@@ -0,0 +1,13 @@
+﻿CALL osae_sp_object_type_add ('LIGHTWAVERF','LightWaveRF Plugin','','PLUGIN',1,1,0,1);
+
+CALL osae_sp_object_type_add ('LIGHTWAVERF DEVICE','LightWaveRF Device','','MULTILEVEL SWITCH',0,0,0,1);
+CALL osae_sp_object_type_state_add ('ON','On','LIGHTWAVERF DEVICE');
+CALL osae_sp_object_type_state_add ('OFF','On','LIGHTWAVERF DEVICE');
+CALL osae_sp_object_type_event_add ('ON','On','LIGHTWAVERF DEVICE');
+CALL osae_sp_object_type_event_add ('OFF','Off','LIGHTWAVERF DEVICE');
+CALL osae_sp_object_type_method_add ('ON','On','LIGHTWAVERF DEVICE','','','','');
+CALL osae_sp_object_type_method_add ('OFF','Off','LIGHTWAVERF DEVICE','','','','');
+CALL osae_sp_object_type_method_add ('HEAT-ON','Heat-On','LIGHTWAVERF DEVICE','','','','');
+CALL osae_sp_object_type_method_add ('HEAT-OFF','Heat-Off','LIGHTWAVERF DEVICE','','','','');
+CALL osae_sp_object_type_property_add ('Room','Integer','','LIGHTWAVERF DEVICE',0);
+CALL osae_sp_object_type_property_add ('Device','Integer','','LIGHTWAVERF DEVICE',0);
