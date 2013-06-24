836cab552843259a582f7674611586ee25bda275
steve.aitken@gmail.com <steve.aitken@gmail.com@68067983-10d6-bc12-f836-432b7b55e70a>
2013-06-24 23:24:10 +0000

    Fix - heating events not correctly state identified, and add heating pages to website.

--- LightwaveSite/Heating.aspx
+++ LightwaveSite/Heating.aspx
@@ -5,4 +5,47 @@
 </asp:Content>
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
 
+    <p>
+        <strong>Boiler Controls:</strong></p>
+    <asp:Panel ID="Panel1" runat="server">
+        Boiler:
+        <asp:RadioButton ID="rbBoilerOn" runat="server" 
+    AutoPostBack="True" oncheckedchanged="rbBoilerOn_CheckedChanged" Text="On" />
+        <asp:RadioButton ID="rbBoilerOff" runat="server" 
+    AutoPostBack="True" oncheckedchanged="rbBoilerOff_CheckedChanged" Text="Off" />
+        <br />
+        HotWater
+        <asp:RadioButton ID="rbHotWaterOn" runat="server" AutoPostBack="True" 
+            oncheckedchanged="rbHotWaterOn_CheckedChanged" Text="On" />
+        <asp:RadioButton ID="rbHotWaterOff" runat="server" AutoPostBack="True" 
+            oncheckedchanged="rbHotWaterOff_CheckedChanged" Text="Off" />
+    </asp:Panel>
+    <strong>
+    <br />
+    Radiator Controls:<br />
+    </strong>
+    <br />
+    Room:
+<asp:DropDownList ID="cmbRoom" runat="server" AutoPostBack="True" 
+        onselectedindexchanged="cmbRoom_SelectedIndexChanged">
+    <asp:ListItem>0</asp:ListItem>
+    <asp:ListItem>1</asp:ListItem>
+    <asp:ListItem>2</asp:ListItem>
+    <asp:ListItem>3</asp:ListItem>
+    <asp:ListItem>4</asp:ListItem>
+    <asp:ListItem>5</asp:ListItem>
+    <asp:ListItem>6</asp:ListItem>
+    <asp:ListItem>7</asp:ListItem>
+</asp:DropDownList>
+<br />
+    Radiator:<br />
+<asp:RadioButton ID="rbOn" runat="server" Text="On" AutoPostBack="True" 
+        oncheckedchanged="rbOn_CheckedChanged" />
+<asp:RadioButton ID="rbOff" runat="server" Text="Off" AutoPostBack="True" 
+        oncheckedchanged="rbOff_CheckedChanged" />
+    <br />
+    <br />
+    Response: 
+    <asp:Label ID="lblResponse" runat="server"></asp:Label>
+    
 </asp:Content>
--- LightwaveSite/Heating.aspx.designer.cs
+++ LightwaveSite/Heating.aspx.designer.cs
@@ -11,5 +11,86 @@ namespace LightwaveSite {
     
     
     public partial class Heating {
+        
+        /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
+        /// rbBoilerOn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbBoilerOn;
+        
+        /// <summary>
+        /// rbBoilerOff control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbBoilerOff;
+        
+        /// <summary>
+        /// rbHotWaterOn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbHotWaterOn;
+        
+        /// <summary>
+        /// rbHotWaterOff control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbHotWaterOff;
+        
+        /// <summary>
+        /// cmbRoom control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cmbRoom;
+        
+        /// <summary>
+        /// rbOn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbOn;
+        
+        /// <summary>
+        /// rbOff control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbOff;
+        
+        /// <summary>
+        /// lblResponse control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblResponse;
     }
 }
